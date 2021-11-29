aa51cbb1fd0f4b40f783f7d7a8ea4f86c08c6373
Matt Roberts <mattrobertsky@users.noreply.github.com>
2021-11-29 17:26:51 +0000

    remove unused

--- app/uk/gov/hmrc/eusubsidycompliancestub/controllers/SubsidyController.scala
+++ app/uk/gov/hmrc/eusubsidycompliancestub/controllers/SubsidyController.scala
@@ -22,7 +22,6 @@ import play.api.mvc.{Action, ControllerComponents}
 import uk.gov.hmrc.eusubsidycompliancestub.models.SubsidyUpdate
 import uk.gov.hmrc.eusubsidycompliancestub.models.json.eis.eisUpdateSubsidyUsageResponse
 import uk.gov.hmrc.eusubsidycompliancestub.models.types.{EORI, SubsidyRef, UndertakingRef}
-import uk.gov.hmrc.eusubsidycompliancestub.services.EisService
 import uk.gov.hmrc.play.bootstrap.backend.controller.BackendController
 
 import scala.concurrent.Future
@@ -30,8 +29,7 @@ import scala.concurrent.Future
 @Singleton
 class SubsidyController @Inject()(
   cc: ControllerComponents,
-  authAndEnvAction: AuthAndEnvAction,
-  eis: EisService
+  authAndEnvAction: AuthAndEnvAction
 ) extends BackendController(cc) {
 
   def updateUsage: Action[JsValue] = authAndEnvAction.async(parse.json) { implicit request =>
