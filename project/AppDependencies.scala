3b57d9f62ea1be99a81d9563c66b46173db471a7
“WitchellJack” <jack.witchell@digital.hmrc.gov.uk>
2023-09-11 17:09:49 +0100

    ESC1221 - Reduced all library versions to last stable version. Upgrade to Scala 2.13 will be bigger job, as Generator that is used is no longer supported

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -9,8 +9,9 @@ object AppDependencies {
 
   val compile = Seq(
     "uk.gov.hmrc"   %% "bootstrap-backend-play-28" % bootStrapVersion,
-    "org.typelevel" %% "cats-core"                 % "2.9.0",
+    "org.typelevel" %% "cats-core"                 % "1.1.0",
     "com.chuusai"   %% "shapeless"                 % "2.3.10",
+    "uk.gov.hmrc"   %% "stub-data-generator"         % "0.5.3",
     "com.github.fge" % "json-schema-validator"     % "2.2.6",
     "wolfendale"    %% "scalacheck-gen-regexp"     % "0.1.2"
   )
@@ -18,11 +19,11 @@ object AppDependencies {
   val test = Seq(
     "org.scalatestplus"      %% "scalacheck-1-15"        % "3.2.11.0"       % Test,
     "com.typesafe.play"      %% "play-test"              % current          % Test,
-    "org.scalacheck"         %% "scalacheck"             % "1.17.0"         % Test,
+    "org.scalacheck"         %% "scalacheck"             % "1.15.4"         % Test,
     "org.scalatestplus.play" %% "scalatestplus-play"     % "5.1.0"          % Test,
     "uk.gov.hmrc"            %% "bootstrap-test-play-28" % bootStrapVersion % Test,
     "org.scalatestplus"      %% "mockito-3-4"            % "3.2.10.0"       % Test,
-    "com.vladsch.flexmark"    % "flexmark-all"           % "0.64.6"         % Test,
+    "com.vladsch.flexmark"    % "flexmark-all"           % "0.60.0"         % Test,
     "wolfendale"             %% "scalacheck-gen-regexp"  % "0.1.2"          % Test
   )
 }
