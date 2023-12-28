3496244b92c60f9d2afcd0a2f5620742df248cdb
feyyazrc <95756811+feyyazrc@users.noreply.github.com>
2023-12-28 09:51:12 +0000

    ESC-1430 Migrate to Play 3.0

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,15 +1,13 @@
 import play.core.PlayVersion.current
-import play.sbt.PlayImport._
-import sbt.Keys.libraryDependencies
 import sbt._
 
 object AppDependencies {
 
-  val bootStrapVersion = "7.15.0"
+  val bootStrapVersion = "8.3.0"
 
   val compile = Seq(
-    "uk.gov.hmrc"   %% "bootstrap-backend-play-28" % bootStrapVersion,
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"    % "1.4.0",
+    "uk.gov.hmrc"   %% "bootstrap-backend-play-30" % bootStrapVersion,
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"    % "1.6.0",
     "org.typelevel" %% "cats-core"                 % "2.9.0",
     "com.chuusai"   %% "shapeless"                 % "2.3.10",
     "com.github.fge" % "json-schema-validator"     % "2.2.6",
@@ -19,12 +17,8 @@ object AppDependencies {
 
   val test = Seq(
     "org.scalatestplus"      %% "scalacheck-1-15"        % "3.2.11.0"       % Test,
-    "com.typesafe.play"      %% "play-test"              % current          % Test,
-    "org.scalacheck"         %% "scalacheck"             % "1.17.0"         % Test,
-    "org.scalatestplus.play" %% "scalatestplus-play"     % "5.1.0"          % Test,
-    "uk.gov.hmrc"            %% "bootstrap-test-play-28" % bootStrapVersion % Test,
-    "org.scalatestplus"      %% "mockito-3-4"            % "3.2.10.0"       % Test,
-    "com.vladsch.flexmark"    % "flexmark-all"           % "0.64.6"         % Test,
+    "org.playframework"      %% "play-test"              % current          % Test,
+    "uk.gov.hmrc"            %% "bootstrap-test-play-30" % bootStrapVersion % Test,
     "wolfendale"             %% "scalacheck-gen-regexp"  % "0.1.2"          % Test
   )
 }
