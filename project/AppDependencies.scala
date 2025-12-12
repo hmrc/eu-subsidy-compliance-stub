544fd77b90cd2fafc13d19ccbdc0b63a78e4c971
Liam-Macarthur <113617571+Liam-Macarthur@users.noreply.github.com>
2025-12-12 15:06:51 +0000

    updating dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,14 +3,14 @@ import sbt._
 
 object AppDependencies {
 
-  val bootStrapVersion = "9.18.0"
+  val bootStrapVersion = "10.4.0"
 
   val compile = Seq(
     "uk.gov.hmrc"   %% "bootstrap-backend-play-30" % bootStrapVersion,
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"    % "2.6.0",
-    "org.typelevel" %% "cats-core"                 % "2.9.0",
-    "com.chuusai"   %% "shapeless"                 % "2.3.10",
-    "com.github.fge" % "json-schema-validator"     % "2.2.6",
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"    % "2.11.0",
+    "org.typelevel" %% "cats-core"                 % "2.13.0",
+    "com.chuusai"   %% "shapeless"                 % "2.3.13",
+    "com.github.fge" % "json-schema-validator"     % "2.2.14",
     "uk.gov.hmrc"   %% "stub-data-generator"       % "1.4.0",
     "wolfendale"    %% "scalacheck-gen-regexp"     % "0.1.2"
   )
